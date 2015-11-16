f736e08ff08b7a9eec8b9ccec4931bf4ae4dd383
Silviya <silviyaranij@deemsysinc.com>
2015-11-16 12:52:23 +0530

    Project Initialised

---
+++ SilviyaApp/SilviyaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SilviyaApp
+//
+//  Created by DeemsysInc on 16/11/15.
+//  Copyright (c) 2015 Deemsys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SilviyaApp/SilviyaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SilviyaApp
+//
+//  Created by DeemsysInc on 16/11/15.
+//  Copyright (c) 2015 Deemsys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
